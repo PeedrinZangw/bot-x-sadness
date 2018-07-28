cda6cd17244d621acb086170e0788fc4f9f30842
PeedrinZangw <39870039+PeedrinZangw@users.noreply.github.com>
2018-07-28 03:01:36 +0000

    Add files via upload

--- bot-x-music.py
+++ bot-x-music.py
@@ -2,15 +2,18 @@ import discord
 import youtube_dl
 from discord.ext import commands
 
-TOKEN = 'NDUyMzQxOTA3MDEyNzE0NTI2.DhyTUg.gkEgwtzKbQTRRbTqDaqy2i2yn9k'
-client = commands.Bot(command_prefix = 's1!')
+TOKEN = 'NDcyNTc5NzAyNDI3NzQ2MzA2.Dj1b7g.ImrLfBRHRq2z56YZO5CFWr0dBrU'
+client = commands.Bot(command_prefix = 'rw!')
 
 players = {}
 
 @client.event
 async def on_ready():
-    print('sadness 01 - Online!')
-    await client.change_presence(game=discord.Game(name='XXXTENTACION - 24/7 [BOT SADNESS 01]'))
+    print('Ratazanas bot - Online')
+    await client.change_presence(game=discord.Game(name='discord.gg/nzbG5eR'))
+    canal = client.get_channel("467101617762730016")
+    msg = "Olá Mundo! :fire:\nAcabei de ser inicializado.\nhttps://i.imgur.com/4yB6I4V.jpg"
+    await client.send_message(canal, msg)
 
 @client.command(pass_context=True)
 async def join(ctx):
@@ -30,5 +33,19 @@ async def play (ctx, url):
     player = await voice_client.create_ytdl_player(url)
     players[server.id] = player
     player.start()
+
+@client.event
+async def on_member_join(member):
+  canal = client.get_channel("467101617762730016")
+  regras = client.get_channel("467103656219508736")
+  msg = "Bem-vindo {} ao Ratazana's World\nAntes de tudo leia as regras no canal {}\n\nAgora você está seguro com a tropa Ratazana.\nhttps://i.imgur.com/4yB6I4V.jpg".format(member.mention, regras.mention)
+  await client.send_message(canal, msg)
+
+@client.event
+async def on_membro_join(member):
+  canal = client.get_channel("467101617762730016")
+  regras = client.get_channel("467103656219508736")
+  msg = "Bem-vindo {} ao Ratazana's World\nAntes de tudo leia as regras no canal {}\n\nAgora você está seguro com a tropa Ratazana.\nhttps://i.imgur.com/4yB6I4V.jpg".format(member.mention, regras.mention)
+  await client.send_message(member, msg)
 
 client.run(TOKEN)
